710f6ed58efcaaa0bb5b9abe32dde524c3553b87
unknown <thosman@Hosman7dt3500.gatan.com>
2013-10-28 16:11:50 -0700

    added ui stuff

--- ROIIntensityTerminate.s
+++ ROIIntensityTerminate.s
@@ -15,24 +15,18 @@ class userScript:object
 	number x,y,z
 	number t,l,b,r
 
-
-
-
-	// init gets executed on startup of script
-	void init(object self)
+	
+	void userScript(object self)
 	{
 		name="ROI intensity termination script"
-		result("SPSCRIPT"+name+": initialized\n")
-		iterations = 0
-		state=1
 	}
-	
-	// dialogParameters() asks for script specific parameters, gets executed before init()
-	void dialogParameters(object self) 
+
+	// init gets executed when script is started
+	void init(object self, number value)
 	{
-		//get user input for intensity change
-		chg = 20
-		getnumber("Specify % intensity change to stop milling and draw ROI with selection tool:",chg,chg)
+		
+		chg = value
+
 		src:=GetFrontImage()
 
 		
@@ -41,7 +35,7 @@ class userScript:object
 		trg:=RGBimage("test",4,x,y)
 		trg=src[0,0,0,x,y,1]
 
-		//let user define ROI
+		//let user define ROI 
 		
 		GetSelection(src,t,l,b,r)
 
@@ -52,15 +46,35 @@ class userScript:object
 		high_thres = startavg * (1+chg/100)
 
 		result("------- Terminate Milling from Image Intensity Change ------------------\n")
-		result(datestamp()+": Starting ROI average intensity="+StartAvg+"\n")
-		result("  Milling will be stopped when intensity changes by "+chg+"%\n")
-		result("      Low  threshold="+low_thres+"\n")
-		result("      High threshold="+high_thres+"\n")
+		result("SPSCRIPT"+datestamp()+": Starting ROI average intensity="+StartAvg+"\n")
+		result("SPSCRIPT  Milling will be stopped when intensity changes by "+chg+"%\n")
+		result("SPSCRIPT      Low  threshold="+low_thres+"\n")
+		result("SPSCRIPT      High threshold="+high_thres+"\n")
+		result("SPSCRIPT"+name+": initialized\n")
+		iterations = 0
+		state=1
+	}
+	
+	// dialogParameters() asks for script specific parameters, gets executed before init() and calls init
+	void dialogParameters(object self) 
+	{
+		result("SPSCRIPT: dialog started\n")
+		//get user input for intensity change
+		//chg = 20
+		//getnumber("Specify % intensity change to stop milling and draw ROI with selection tool:",chg,chg)
+		
+		// dialog object created, this object will call init when parameters are set
+		object dlg = alloc(ParameterEntryDialog).init(self)
+		dlg.display("Test Dialog")
+
+		
 
 
 		//now call init()
-		self.init()
+		//self.init()
 	}
+
+
 
 	
 	
@@ -68,6 +82,7 @@ class userScript:object
 	void terminate(object self)
 	{
 		TagGroupSetTagAsNumber( GetPersistentTagGroup(),"SPScript:listener running", 0 )
+		PIPS_stoprecipe()
 		result("SPSCRIPT: terminate sent\n")
 	}
 	
@@ -108,7 +123,14 @@ class userScript:object
 
 	void pause(object self)
 	{
-
+		PIPS_PauseRecipe()
+		result("SPSCRIPT: script paused\n")
+	}
+	
+	void resume(object self)
+	{
+		PIPS_ResumeRecipe()
+		result("SPSCRIPT: script resumed\n")
 	}
 
 }
@@ -116,6 +138,8 @@ class userScript:object
 
 //object test1 = alloc(userScript)
 //test1.dialogParameters()
+
+
 //test1.init()
 //test1.SPSCRIPT_init()
 
@@ -124,6 +148,106 @@ class userScript:object
 
 
 
+
+
+
+
+
+
+
+class ParameterEntryDialog : uiframe
+{
+	TagGroup 	PSDialog, PSDialogItems
+	Object		PSDialogWindow
+	taggroup	D1, D2
+	taggroup 	f1, f2	
+	object parent
+	number val1
+			
+	object init(object self, object aParent)
+	{
+		result("PARAMETERENTRYDIALOG: initialized\n")
+		PsDialog = DLGCreateDialog("Enter parameters", PSDialogItems)
+		
+		// create items and add them to dialog
+		D1 = DLGCreateIntegerField(1)
+		f1 = DLGCreateIntegerField("Specify % intensity change to stop milling and draw ROI with selection tool:", D1, 1,5)
+		PSDialog.DLGAddElement(f1)
+		
+		
+		//D2 = DLGCreateIntegerField(1)
+		//f2 = DLGCreateRealField(" parameter 2", D2, 2,5,5)
+		//PSDialog.DLGAddElement(f2)
+		//Number val2 = D2.DLGGetValue()
+		
+		parent = aParent
+		
+		//PSDialog.DLGTableLayout(1,2,0)
+		
+		taggroup runButtonTags = DLGCreatePushButton("Run", "OnButtonPressedRun")
+		taggroup pauseButtonTags = DLGCreatePushButton("Pause", "OnButtonPressedPause")
+		taggroup resumeButtonTags = DLGCreatePushButton("Resume", "OnButtonPressedResume")
+		taggroup stopButtonTags = DLGCreatePushButton("Manual Stop", "OnButtonPressedStop")
+		PsDialog.DLGAddElement(runButtonTags)
+		PsDialog.DLGAddElement(pauseButtonTags)
+		PsDialog.DLGAddElement(resumeButtonTags)
+		PsDialog.DLGAddElement(stopButtonTags)
+		dlgenabled(runButtonTags,1)
+		dlgenabled(pauseButtonTags,0)
+		dlgenabled(resumeButtonTags,0)
+		dlgenabled(stopButtonTags,0)
+		dlgidentifier(runButtonTags, "run")
+		dlgidentifier(pauseButtonTags, "pause")
+		dlgidentifier(resumeButtonTags, "resume")
+		dlgidentifier(stopButtonTags, "stop")
+		
+		
+
+		//Number bin = Dbin.DLGGetValue()
+		return self.super.init(PSDialog)
+		
+	}
+		
+	void OnButtonPressedRun(object self)
+	{
+
+		val1 = D1.DLGGetValue()
+		parent.init(val1)
+		self.setelementisenabled("run", 0);
+		self.setelementisenabled("pause", 1);
+		self.setelementisenabled("resume", 1);
+		self.setelementisenabled("stop", 1);
+		
+	}
+
+	void OnButtonPressedPause(object self)
+	{
+
+		parent.pause()
+		
+	}
+
+	void OnButtonPressedResume(object self)
+	{
+
+		parent.resume()
+		
+	}
+	
+	void OnButtonPressedStop(object self)
+	{
+
+		parent.terminate()
+		self.setelementisenabled("run", 1);
+		
+	}
+
+	void display(object self, string title)
+	{
+		self.super.display(title)
+	}			
+		
+}
 
 
 
